8c6907767aa0ddad8d74287d2fc59ee3a37ffa61
Ryan Brower <ryan.brower28@gmail.com>
2022-06-30 13:30:03 -0700

    connecting database with it's own class

--- database.py
+++ database.py
@@ -1,13 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from recipe import Recipe
 
 cred = credentials.Certificate("cloud-recipes-rbrower-d83cf795140d.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-# doc_ref = db.collection(u'recipes').document(u'Bread')
+doc_ref = db.collection(u'recipes').document(u'Bread')
+
 # doc_ref.set({
 #     u'1': u'Flour',
 #     u'2': u'Water',
@@ -17,6 +19,21 @@ db = firestore.client()
 #     u'6': u'Egg',
 #     u'7': u'Oil'
 # })
+
+bread = Recipe(
+    "Bread",
+    ["Flour", "Water", "Yeast", "Milk", "Salt", "Egg", "Oil"],
+    {
+        1: "Stir together dry ingredients.",
+        2: "Add water, egg and oil and stir.",
+        3: "Knead dough until spongy but firm.",
+        4: "Bake until browned and cripsy on the outside."
+    }
+)
+
+doc_ref.set(bread.to_dict())
+
+
 
 recipes_ref = db.collection(u'recipes')
 docs = recipes_ref.stream()
--- recipe.py
+++ recipe.py
@@ -5,3 +5,28 @@ class Recipe:
         self.name = name
         self.ingredients = ingredients
         self.instructions = instructions
+
+
+    def from_dict(self, source):
+        self.name = list(source.keys())[0]
+        self.ingredients = source.ingredients
+        self.instructions = source.instructions
+
+
+    def to_dict(self):
+        return {
+            self.name: {
+                "ingredients": self.ingredients,
+                "instructions": self.instructions
+            }
+        }
+
+
+    def __repr__(self):
+        return(
+            f'City(\
+                name={self.name}, \
+                ingredients={self.ingredients}, \
+                instructions={self.instructions} \
+            )'
+        )
